2f475849a60736684a49f778d27592584ca126dd
AK Raid <akraid@gmail.com>
2026-05-24 11:04:33 -0600

    Learning to commit and push

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'In the Deep and the Dark',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
