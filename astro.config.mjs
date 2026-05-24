06a3e6d53f510809503e64ef95bf56c9ee2f343a
AK Raid <akraid@gmail.com>
2026-05-24 11:14:03 -0600

    Resetting back to default astro install.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'In the Deep and the Dark',
+			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
